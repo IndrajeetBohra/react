7b7abb8926f3dade0546431a37aa6fd960d58409
IndrajeetBohra <bohraindrajeet@gmail.com>
2026-04-20 00:38:48 +0530

    Separate into smaller components

---
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Header } from "../../components/Header";
+import { ProductsGrid } from "./ProductsGrid";
+import "./HomePage.css";
+
+export function HomePage({ cart }) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/products").then((response) => {
+      setProducts(response.data);
+    });
+  }, []); // dependecy array. when empty only runs once
+
+  // .then is used by promises as it is async
+  // fetch returns a promise as the request takes time
+  // similarly .json() is also async i.e it takes time
+
+  return (
+    <>
+      <link
+        rel="icon"
+        type="image/svg+xml"
+        href="/home-favicon.png
+      "
+      />
+
+      <title>Ecommerce Project</title>
+
+      <Header cart={cart} />
+      <div className="home-page">
+        <ProductsGrid products={products} />
+      </div>
+    </>
+  );
+}
