dac7c0e4855a671003ca6ff93d1fb94db5236d8c
IndrajeetBohra <bohraindrajeet@gmail.com>
2026-04-20 17:05:38 +0530

    Convert home and checkout to async await.

--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -8,9 +8,11 @@ export function HomePage({ cart }) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    axios.get("/api/products").then((response) => {
+    const getHomeData = async () => {
+      const response = await axios.get("/api/products");
       setProducts(response.data);
-    });
+    };
+    getHomeData();
   }, []); // dependecy array. when empty only runs once
 
   // .then is used by promises as it is async
